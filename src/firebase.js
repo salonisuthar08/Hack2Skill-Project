6bd43342b63c955752d936c5b908ed95af5520f3
Mehul Suthar <154885299+Mehulsuthar817@users.noreply.github.com>
2025-09-01 19:26:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRb-uF44NEus7vDrTB4G6z7XRt7tvPKLI",
-  authDomain: "soulcraft-7975d.firebaseapp.com",
-  projectId: "soulcraft-7975d",
-  storageBucket: "soulcraft-7975d.firebasestorage.app",
-  messagingSenderId: "365372739733",
-  appId: "1:365372739733:web:f48159f2c5565bcaa2cb9c",
-  measurementId: "G-MJEL5DQ2EH"
+firebase SDK
 };
 
 // Initialize Firebase
